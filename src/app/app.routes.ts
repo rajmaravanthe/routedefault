decada7190a5570a1c1ff2fdb9baf63076391d72
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-04-19 16:34:21 +0530

    added new files

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Route } from '@angular/router';
+import { ViewComponent } from './view/view.component';
+import { AddComponent } from './add/add.component';
+
+const routes: Route[] = [
+    { path: '', component: AddComponent },
+    { path: 'view', component: ViewComponent },
+    { path: 'add', component: AddComponent }
+]
+
+export const RoutesProvider = RouterModule.forRoot(routes);
